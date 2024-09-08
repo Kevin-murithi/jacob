3514fd64ec2bb8b079d86254fee8f0bd222793c0
kevin-murithi <murithikevin54@gmail.com>
2024-09-08 17:07:05 +0300

    commit

--- app.js
+++ app.js
@@ -15,7 +15,7 @@ const port = process.env.PORT || 3000;
 app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
 app.use(cors({
-    origin: 'https://trialproject-wine.vercel.app', // Your frontend URL
+    origin: 'https://jacob.railway.internal.app', // Your frontend URL
     methods: ['GET', 'POST'],
     credentials: true
 }));
