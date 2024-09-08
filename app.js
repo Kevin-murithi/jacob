bdffccf1e8f72f0d82b4bf4edcfe60a67ab5680b
kevin-murithi <murithikevin54@gmail.com>
2024-09-08 12:28:56 +0300

    commit

--- app.js
+++ app.js
@@ -24,13 +24,7 @@ app.use(session({
 }));
 
 // Create a MySQL connection using environment variables
-const db = mysql.createConnection(process.env.MYSQLURL || {
-    host: process.env.MYSQLHOST,
-    user: process.env.MYSQLUSER,
-    password: process.env.MYSQLPASSWORD,
-    database: process.env.MYSQLDATABASE,
-    port: process.env.MYSQLPORT
-});
+const db = mysql.createConnection(process.env.MYSQLURL);
 
 // Connect to the MySQL database
 db.connect((err) => {
